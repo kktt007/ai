bf62473f21b4d213e5fdf1eb3fcaae46f4da9d92
kktt007 <kktt007.top@gmail.com>
2021-09-10 19:54:35 +0800

    Add files via upload

---
+++ ai.sh
@@ -0,0 +1,5 @@
+curl -O https://raw.githubusercontent.com/kktt007/ai/main/preinstall.sh
+curl -O https://raw.githubusercontent.com/kktt007/ai/main/chroot.sh
+
+chmod +x preinstall.sh
+chmod +x chroot.sh

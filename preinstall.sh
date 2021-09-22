b16257bb57343fcc4af0291951cc7e243cea0311
kktt007 <kktt007.top@gmail.com>
2021-09-22 16:12:24 +0800

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,6 +1,6 @@
-echo 'Server = https://mirrors.cnnic.cn/archlinux/$repo/os/$arch' > /etc/mirror.d/mirrorlist
-echo 'Server = http://mirrors.163.com/archlinux/$repo/os/$arch' >> /etc/mirror.d/mirrorlist
-echo 'Server = http://mirrors.aliyun.com/archlinux/$repo/os/$arch' >> /etc/mirror.d/mirrorlist
+echo 'Server = https://mirrors.cnnic.cn/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+echo 'Server = http://mirrors.163.com/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
+echo 'Server = http://mirrors.aliyun.com/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
 pacman -Sy
 # pacstrap /mnt reflector nano
 # reflector --country China --age 12 --protocol https --ipv4 -x dgut.edu.cn -x sjtu.edu.cn --download-timeout 30 --sort rate --fastest 2 --save /etc/pacman.d/mirrorlist

60cbfd7fbd438de8d7e3867ef4be12864b21a228
kktt007 <kktt007.top@gmail.com>
2021-09-23 19:47:36 +0800

    Update i3.sh

--- i3.sh
+++ i3.sh
@@ -1,10 +1,7 @@
 sudo pacman -S --noconfirm go python python-pip python-pipenv perl ruby xorg-server xorg-server-common xorg-xrandr arandr xorg-xrdb xorg-xmodmap xkeycaps xdg-user-dirs ttf-joypixels wqy-microhei xf86-input-libinput alsa-utils pulseaudio pulseaudio-alsa alsa-plugins alsa-firmware kmix pavucontrol-qt libx11 libxext nvidia nvidia-utils nvidia-settings libva-vdpau-driver libva-utils libva-intel-driver zsh zsh-autosuggestions zsh-syntax-highlighting zsh-completions bash-completion rxvt-unicode p7zip unrar tar unzip unarchiver tftp-hpa fd neovim cscope vim-jedi xsel nodejs yarn meld tmux weechat neofetch flameshot mpv smplayer gstreamer gst-libav gst-plugins-good fcitx-im fcitx-rime kcm-fcitx fcitx-table-extra fcitx-im ripgrep stow zathura zathura-pdf-mupdf openssh simplescreenrecorder bleachbit papirus-icon-theme perl-xml-parser bin86 asciidoc b43-fwcutter cdrtools mercurial fastjar intltool sharutils time xmlto qemu upx dtc lib32-glibc msmtp uglify-js libantlr3c gperf swig rsync sddm
 
 # i3
-sudo pacman -S --noconfirm rofi dunst numlockx network-manager-applet flameshot ranger
-
-# kde
-# kf5 kf5-aids plasma packagekit-qt5 dolphin kcron khelpcenter ksystemlog partitionmanager konsole filelight kdf kgpg kate kbackup markdownpart kdialog kcharselect sweeper gwenview kcolorchooser okular kruler kdeconnect kdenetwork-filesharing kget materia-kde kvantum-theme-materia
+sudo pacman -S --noconfirm i3-gaps rofi dunst numlockx network-manager-applet flameshot ranger
 
 cd ~
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
@@ -28,11 +25,7 @@ sudo echo -e "export GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport XMOD
 yay nerd-fonts-meslo
 yay nerd-fonts-dejavu-complete
 yay ttf-sarasa-gothic
-# yay nerd-fonts-fira-code
-# yay brave-bin
-# yay visual-studio-code-bin
-# yay appimagelauncher
-# yay plasma5-applets-window-appmenu
-
-# balooctl suspend
-# balooctl disable
+yay nerd-fonts-fira-code
+yay brave-bin
+yay visual-studio-code-bin
+yay appimagelauncher

8f8fe3f13a3c4694a095d32fbda9130e1d7b9807
kktt007 <kktt007.top@gmail.com>
2021-09-24 14:37:42 +0800

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,12 +1,12 @@
 # echo 'Server = https://mirrors.cnnic.cn/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
 # echo 'Server = http://mirrors.163.com/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
 # echo 'Server = http://mirrors.aliyun.com/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
-pacstrap /mnt reflector nano
-reflector --country China --age 12 --protocol https --ipv4 -x dgut.edu.cn -x sjtu.edu.cn --download-timeout 30 --sort rate --fastest 2 --save /etc/pacman.d/mirrorlist
+pacstrap /mnt reflector
+reflector --country China --age 12 --protocol https --ipv4 -x dgut.edu.cn -x sjtu.edu.cn --download-timeout 30 --sort rate --fastest 3 --save /etc/pacman.d/mirrorlist
 
-sed -i "1iServer = https://mirrors.cnnic.cn/archlinux/$repo/os/$arch" /etc/pacman.d/mirrorlist
-sed -i "2iServer = http://mirrors.163.com/archlinux/$repo/os/$arch" /etc/pacman.d/mirrorlist
-sed -i "3iServer = http://mirrors.aliyun.com/archlinux/$repo/os/$arch" /etc/pacman.d/mirrorlist
+sed -i "11iServer = https://mirrors.cnnic.cn/archlinux/\$repo/os/\$arch" /etc/pacman.d/mirrorlist
+sed -i "12iServer = http://mirrors.163.com/archlinux/\$repo/os/\$arch" /etc/pacman.d/mirrorlist
+sed -i "13iServer = http://mirrors.aliyun.com/archlinux/\$repo/os/\$arch" /etc/pacman.d/mirrorlist
 
 pacman -Sy
 

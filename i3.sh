616062a4698aae498b31d3aec409e3fe522c0917
kktt007 <kktt007.top@gmail.com>
2021-09-24 16:50:36 +0800

    Update i3.sh

--- i3.sh
+++ i3.sh
@@ -1,6 +1,5 @@
 sudo pacman -S --noconfirm go python python-pip python-pipenv perl ruby xorg-server xorg-server-common xorg-xrandr arandr xorg-xrdb xorg-xmodmap xkeycaps xdg-user-dirs ttf-joypixels wqy-microhei xf86-input-libinput alsa-utils pulseaudio pulseaudio-alsa alsa-plugins alsa-firmware kmix pavucontrol-qt nvidia nvidia-utils nvidia-settings libva-vdpau-driver libva-utils libva-intel-driver zsh zsh-autosuggestions zsh-syntax-highlighting zsh-completions bash-completion rxvt-unicode p7zip unrar tar unzip unarchiver tftp-hpa fd neovim cscope vim-jedi xsel nodejs yarn meld tmux weechat neofetch flameshot mpv smplayer gstreamer gst-libav gst-plugins-good fcitx-im fcitx-rime kcm-fcitx fcitx-table-extra ripgrep stow zathura zathura-pdf-mupdf openssh simplescreenrecorder bleachbit papirus-icon-theme libx11 libxext perl-xml-parser bin86 asciidoc b43-fwcutter cdrtools mercurial fastjar intltool sharutils time xmlto qemu upx dtc lib32-glibc msmtp uglify-js libantlr3c gperf swig rsync sddm
 
-
 # i3
 sudo pacman -S --noconfirm i3-gaps rofi dunst numlockx network-manager-applet flameshot ranger
 
@@ -14,8 +13,8 @@ cd ~
 curl -O curl -O https://kktt007.top/ai/optimus.conf
 sudo mkdir -p /etc/optimus-manager && cp optimus.conf /etc/optimus-manager/optimus-manager.conf
 
-sudo systemctl enable sddm.service optimus-manager.service
-sudo systemctl enable sddm.service sddm.service
+sudo systemctl enable sddm.service 
+sudo systemctl enable optimus-manager.service
 
 # optimus-manager --set-startup=nvidia
 

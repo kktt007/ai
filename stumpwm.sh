4dcbc4779271ec10275ea630d4479f68a9190594
kktt007 <kktt007.top@gmail.com>
2021-12-22 09:33:01 +0800

    Update stumpwm.sh

--- stumpwm.sh
+++ stumpwm.sh
@@ -33,4 +33,4 @@ yay stumpwm
 yay nerd-fonts-meslo
 yay nerd-fonts-dejavu-complete
 
-sudo echo -e "[Desktop Entry]\nEncoding=UTF-8\nName=StumpWM\nComment=Login using\nStumpWM\nExec=stumpwm\nType=XSession" > /usr/share/xsessions/ stumpwm.desktop
+sudo echo -e "[Desktop Entry]\nEncoding=UTF-8\nName=StumpWM\nComment=Login using\nStumpWM\nExec=stumpwm\nType=XSession" > /usr/share/xsessions/stumpwm.desktop

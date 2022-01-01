2fd9a216fbf1ec0e5873605058919ca972f97784
KKTT007 <kktt007.top@gmail.com>
2022-01-01 19:45:11 +0800

    partition

--- oss/ai.sh
+++ oss/ai.sh
@@ -1,5 +1,7 @@
 curl -O https://kktt007.top/ai/preinstall.sh
 curl -O https://kktt007.top/ai/chroot.sh
+curl -O https://kktt007.top/ai/partition.sh
 
 chmod +x preinstall.sh
 chmod +x chroot.sh
+chmod +x partition.sh
---
+++ oss/partition.sh
@@ -0,0 +1,15 @@
+mkfs.vfat -F32 /dev/sda1
+
+mkfs.ext4 /dev/sda3
+
+mkswap /dev/sda2
+
+swapon /dev/sda2
+
+mkfs.ext4 /dev/sda3
+
+mount /dev/sda3 /mnt
+
+mkdir /mnt/boot
+
+mount /dev/sda1 /mnt/boot
